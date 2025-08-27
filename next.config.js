ab4e18cc26d2cd3245f720876ac261cb9d84fcfd
Stakeados Team <stake@stakeados.com>
2025-08-27 05:36:01 +0200

    Add Netlify configuration for proper Next.js deployment

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    domains: ['images.pexels.com', 'res.cloudinary.com'],
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
